d5d8863d21b79ba5c59bb33ba6ef7f5c1d9b1254
Gruia Pana <gruiapana99@gmail.com>
2025-02-10 12:54:15 +0200

    Initial .NET 8 API with User model and DB setup

---
+++ src/OnboardingAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnboardingAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } // Admin, Employee
+    }
+}
